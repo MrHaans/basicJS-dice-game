5553900511fbbbd2cbd2e61fccf6eb4c9c9e0207
MrHaans <aansfarhans@gmail.com>
2022-03-17 22:58:31 +0700

    first commit

---
+++ Dice Game.js
@@ -0,0 +1,7 @@
+let dice1 = Math.floor(Math.random() * 6);
+let dice2 = Math.floor(Math.random() * 6);
+let total = dice1 + dice2;
+
+console.log(`First dice is ${dice1 += 1}.`);
+console.log(`Second dice is ${dice2 += 1}.`);
+console.log(`Total : ${total += 2}.`);
